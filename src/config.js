98232770d7e8c1e00c1183fa040789bdba34719c
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-05-07 18:16:24 -0400

    change API endpoint url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line
 export default {
-  API_ENDPOINT: 'http://localhost:8000'
+  // API_ENDPOINT: 'http://localhost:8000'
+  API_ENDPOINT: 'https://nameless-atoll-55831.herokuapp.com/'
 }
